a789f618fffb6f04f4f5104039431d8d54d5a0d0
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2023-04-23 21:05:59 -0600

    Multiple Update
    Dockerfile:
     - Moved Networks
     - Updated the MongoService to have a volume. The volume will persist data so you do not need to re-enter it every time.
    
    Product:
    - Added the ID attribute
    - Added the collection for products
    - Changed the UUID from string back to UUID
    - Added default constructor to make life easier
    
    ProductController:
    - Removed anything that set the UUID (it's now handled by the constructor)
    - Updated methods
    
    ProductRepository:
    - Removed redundant code (save, findbyId, deleteBNyProductUUID. They are default methods built in by MONGO so no need to redefine them.
    - Changed the list to have the public field (unnecessary but perty)
    
    Application.properties:
    - Changed from "localhost" to "FinalSEN300ProductsServiceDBMongo" that way it connects to the mongo container on the dockerized network

--- FinalProductService/src/main/java/sen300/FinalProductService/Product.java
+++ FinalProductService/src/main/java/sen300/FinalProductService/Product.java
@@ -1,19 +1,30 @@
 package sen300.FinalProductService;
-import org.springframework.data.annotation.Id;
+import java.util.*;
 
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
 public class Product {
-    //@Id
-    private String productUUID;
+    
+    @Id
+    private UUID productUUID;
     private String productName;
     private double price;
     private String description;
 
+    public Product() {
+        this.productUUID = UUID.randomUUID();
+        this.productName = null;
+        this.price = 0;
+        this.description = null;
+    }
 
     //getters and setters
-    public String getProductUuid() {
+    public UUID getProductUUID() {
         return productUUID;
     }
-    public void setProductUuid(String productUUID) {
+    public void setProductUUID(UUID productUUID) {
         this.productUUID = productUUID;
     }
     public String getProductName() {
